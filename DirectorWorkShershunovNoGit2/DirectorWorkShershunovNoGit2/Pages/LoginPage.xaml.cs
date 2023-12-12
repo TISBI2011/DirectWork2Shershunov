f02a93ce14f30ade18c79fbf3aa296482f174cc5
ИС201-1 <IS201-1@tisbi.ru>
2023-12-12 16:47:28 +0300

    privet

---
+++ DirectorWorkShershunovNoGit2/DirectorWorkShershunovNoGit2/Pages/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DirectorWorkShershunovNoGit2.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirectorWorkShershunovNoGit2
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void TBLogin_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void TBVxod_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BLogin_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage());
+            var user = App.DB.User.FirstOrDefault(x => x.Login == TBLogin.Text && x.Password == TBPassword.Text);
+
+            if (user != null)
+                NavigationService.Navigate(new MainPage());
+            else
+                MessageBox.Show("Введены неверные данные");
+        }
+
+        private void BReg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
